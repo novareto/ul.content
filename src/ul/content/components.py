2965aa77f1dee3ea8424520437955bb1ee0c5690
Souheil CHELFOUH <trollfot@gmail.com>
2014-10-13 15:35:20 +0200

    Added NameChooser and fixed imports

--- src/ul/content/components.py
+++ src/ul/content/components.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from unidecode import unidecode
 from cromlech.container.components import Container as BaseContainer
 from cromlech.container.contained import Contained
 from cromlech.container.interfaces import INameChooser
 from uvc.content import schematic_bootstrap, schema
+from uvc.interfaces import IContent, IContainer, IDescriptiveSchema
 from zope.interface import implementer
-from .interfaces import Icontent, IContainer, IDescriptiveSchema
+from grokcore.component import context
 
 
 @implementer(IContent)
@@ -22,3 +24,42 @@ class Container(BaseContainer):
     """
     schema(IDescriptiveSchema)
     __init__ = schematic_bootstrap
+
+
+@implementer(INameChooser)
+class NormalizingNamechooser(Adapter):
+    context(IContainer)
+
+    retries = 100
+
+    def __init__(self, context):
+        self.context = context
+
+    def checkName(self, name, object):
+        return not name in self.context
+
+    def _findUniqueName(self, name, object):
+        if not name in self.context:
+            return name
+
+        idx = 1
+        while idx <= self.retries:
+            new_name = "%s_%d" % (name, idx)
+            if not new_name in self.context:
+                return new_name
+            idx += 1
+
+        raise ValueError(
+            "Cannot find a unique name based on "
+            "`%s` after %d attempts." % (name, self.retries))
+
+    def chooseName(self, name, object):
+        if not name:
+            dc = IDescriptiveSchema(object, None)
+            if dc is not None and dc.title:
+                name = dc.title.strip()
+                name = unidecode(name).strip().replace(' ', '_').lower()
+            else:
+                name = object.__class__.__name__.lower()
+
+        return self._findUniqueName(name, object)
